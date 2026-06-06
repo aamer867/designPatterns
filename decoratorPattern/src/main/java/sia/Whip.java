2f781c6c2018cb3c610d72a07d16d9d1e89a2699
mo <mohamedaamer867@gmail.com>
2026-06-06 17:34:48 +0200

    Add: all condiments

--- decoratorPattern/src/main/java/sia/Whip.java
+++ decoratorPattern/src/main/java/sia/Whip.java
@@ -4,4 +4,13 @@ public class Whip extends CondimentDecorator {
     public Whip(Beverage beverage) {
         super(beverage);
     }
+    @Override
+    public float cost() {
+        return getBeverage().cost() + 0.29f;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Mocha";
+    }
 }
